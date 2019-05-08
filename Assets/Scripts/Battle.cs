6b2ad223bb33e58d72e1703254687e72b7b64d1b
Femi Oladipupo <imef96@gmail.com>
2019-05-08 12:27:51 -0400

    Create battle and monster classes

---
+++ Assets/Scripts/Battle.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour {
+
+  // 0 front, 1 middle, 2 back
+  public Monster[] sideLeft;
+  public Monster[] sideRight;
+  public List<Monster> monsters;
+
+  void Start() {
+    sideLeft = new Monster[3];
+    sideRight = new Monster[3];
+  }
+
+  void Update() {}
+
+
+  public void ActionWait() {
+    GetNextTurn();
+  }
+
+  private void MoveMonster(int monster, int dir) {
+    int side = monster / 3; // 0 is left, 1 is right
+    int pos = monster % 3;
+
+    Monster movingMon;
+
+    while (dir != 0) {
+      if ((dir > 0 && pos == 2) || (dir < 0 && pos == 0)) {
+        dir = 0;
+        continue;
+      }
+
+      int i = (int)Mathf.Sign(dir);
+
+      if (side == 0) {
+        movingMon = sideLeft[pos];
+        sideLeft[pos] = sideLeft[pos + i];
+        sideLeft[pos + i] = movingMon;
+        pos += i;
+      } else {
+        movingMon = sideRight[pos];
+        sideRight[pos] = sideRight[pos + i];
+        sideRight[pos + i] = movingMon;
+        pos += i;
+      }
+    }
+  }
+
+  private void ResetInitiative() {
+    foreach (Monster m in monsters) {
+      m.currentInitiative = Random.Range(1, 9) + m.initiative;
+    }
+  }
+
+  private void GetNextTurn() {
+    GetNextMonster();
+  }
+
+  private Monster GetNextMonster() {
+    int bestInit = 0;
+    List<Monster> bestMons = new List<Monster>();
+
+    foreach (Monster m in monsters) {
+      if (m.currentInitiative == bestInit) {
+        bestMons.Add(m);
+      }
+
+      if (m.currentInitiative > bestInit) {
+        bestMons = new List<Monster>();
+        bestMons.Add(m);
+      }
+    }
+
+    if (bestInit == 0) {
+      ResetInitiative();
+      return GetNextMonster();
+    }
+
+    Monster bestMon = bestMons[Random.Range(0, bestMons.Count)];
+    bestMon.currentInitiative = 0;
+    return bestMon;
+  }
+
+  private void UpdateMonsters() {
+    monsters = new List<Monster>();
+
+    foreach (Monster m in sideLeft) {
+      if (m != null)
+        monsters.Add(m);
+    }
+
+    foreach (Monster m in sideRight) {
+      if (m != null)
+        monsters.Add(m);
+    }
+  }
+}
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EType {
+  Normal, Fire
+}
+
+public class Monster {
+
+  // Biographic info
+  public string name;
+  public int speciesID;
+  public EType type;
+
+  // Attributes
+  public int str;
+  public int agi;
+  public int wis;
+  public int rea;
+  public int vit;
+  public int wil;
+
+  // Derives stats
+  public int maxHealth;
+  public int initiative;
+  public int evasion;
+  public int vitalResist;
+  public int mentalResist;
+  public int normalResist;
+
+  // Battle stats
+  public int currentInitiative = 0;
+
+  public Monster(string name, int species) {
+    this.name = name;
+    this.speciesID = species;
+
+    type = EType.Normal;
+    str = 5;
+    agi = 5;
+    wis = 5;
+    rea = 5;
+    vit = 5;
+    wil = 5;
+
+    CalculateCurrentStats();
+  }
+
+  private void CalculateCurrentStats() {
+    maxHealth = 20 + (vit * 3) + str;
+    initiative = 0 + rea;
+    evasion = 0 + agi + rea;
+    vitalResist = vit;
+    mentalResist = wis;
+    normalResist = wil;
+  }
+}
